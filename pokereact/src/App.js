0eb6ccad50b2d8ad5b720ec9c6c7a82420ca6631
mmal73 <luisito250899@gmail.com>
2020-11-13 02:39:43 -0600

    feat(Components): important pages added

--- pokereact/src/App.js
+++ pokereact/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+} from "react-router-dom";
+import { Home } from './components/home/Home';
+import { Favorites } from './components/favorites/Favorites';
+import { AboutUs } from './components/aboutUs/AboutUs';
+import { Layout } from './components/layout/Layout';
+import { AppRouter } from './router/AppRouter';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <AppRouter/>
+      <Layout />
+    </>
   );
 }
 
